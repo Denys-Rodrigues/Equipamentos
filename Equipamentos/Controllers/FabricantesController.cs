5ce308e0010f5c5256af1dbf5238145be133c518
DW Rodrigues <denys.w.r@hotmail.com>
2021-08-30 21:56:35 -0300

    Criação da categoria "Fabricante" e conexão com o Banco de Dados.

---
+++ Equipamentos/Controllers/FabricantesController.cs
@@ -0,0 +1,20 @@
+﻿using Equipamentos.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Equipamentos.Controllers
+{
+    public class FabricantesController : Controller
+    {
+        private EFContext context = new EFContext();
+
+        // GET: Fabricantes
+        public ActionResult Index()
+        {
+            return View(context.fabricantes.OrderBy(f => f.Nome));
+        }
+    }
+}
